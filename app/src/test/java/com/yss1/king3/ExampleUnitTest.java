1e3a22e3e939bb11b1f9f70b0edc0b604cdd7060
yss120612 <yss1@yandex.ru>
2021-08-04 18:01:52 +0800

    g1

--- app/src/test/java/com/yss1/king3/ExampleUnitTest.java
+++ app/src/test/java/com/yss1/king3/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.yss1.king3;
 
+import com.yss1.lib_jm.NetPacket;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,5 +15,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void netpacked_isCorrect() {
+        NetPacket np= new NetPacket();
+        np.set_send_to(NetPacket.AddressType.ALLNOTME);
+        np.set_sender(NetPacket.AddressType.DOWN_USER);
+        np.setContentType(NetPacket.PType.IM_ALIVE);
+        np.setContent("HUUU");
+        //np.prepare2sendPacket().get("RECPT")
+        assertEquals((String)np.prepare2sendPacket().get("RCPT"), "A");
     }
 }
